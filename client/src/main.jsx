656ffe6883328c6d28d5cc9f9a983c0a1dc85bf2
Melissa Wright <meliswright925@gmail.com>
2024-04-22 00:58:49 -0400

    client src utils files

---
+++ client/src/main.jsx
@@ -0,0 +1,37 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { ApolloProvider } from "@apollo/client";
+import App from "./App.jsx";
+import client from "./graphql/apollo_client.js";
+import SavedBooks from "./pages/SavedBooks";
+import SearchBooks from "./pages/SearchBooks";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    
+    errorElement: <h1 className="display-2">Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <SearchBooks />,
+      },
+      {
+        path: "/saved",
+        element: <SavedBooks />,
+
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <RouterProvider router={router} />
+  </ApolloProvider>
+);
+
+
